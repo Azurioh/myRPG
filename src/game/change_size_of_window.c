b7d3bd4028dacf9221cb03af564d05b6168d55db
Azurioh <alan.cunin@epitech.eu>
2024-04-22 23:32:03 +0200

    (~) Update function to change the size of the window File modified: src/game/change_size_of_window.c

--- src/game/change_size_of_window.c
+++ src/game/change_size_of_window.c
@@ -7,22 +7,32 @@
 
 #include "../../include/myrpg.h"
 
-int change_size_of_window(game_t *game_info, int add)
+int increase_size_of_window(game_t *game_info)
 {
     if (!game_info
         || !game_info->window
-        || !game_info->window_size_list
-        || !game_info->actual_window_size)
+        || !game_info->window_size_list) {
         return -1;
-    if (add == 0) {
-        if (game_info->actual_window_size == 0)
-            return 0;
-        game_info->actual_window_size--;
-    } else {
-        if (game_info->actual_window_size == 1)
-            return 0;
-        game_info->actual_window_size++;
     }
+    if (game_info->actual_window_size == 2) {
+        return 0;
+    }
+    game_info->actual_window_size++;
+    init_window(game_info);
+    return 0;
+}
+
+int decrease_size_of_window(game_t *game_info)
+{
+    if (!game_info
+        || !game_info->window
+        || !game_info->window_size_list) {
+        return -1;
+    }
+    if (game_info->actual_window_size == 0) {
+        return 0;
+    }
+    game_info->actual_window_size--;
     init_window(game_info);
     return 0;
 }
