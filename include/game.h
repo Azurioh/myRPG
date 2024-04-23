afb187b712fba983bc61ce96c25d5e49c91d8f81
Azurioh <alan.cunin@epitech.eu>
2024-04-23 19:17:35 +0200

    (~) Update the game.h include file by adding some protos and add a value in the game struct File modified: include/game.h

--- include/game.h
+++ include/game.h
@@ -2,17 +2,19 @@
 ** EPITECH PROJECT, 2024
 ** game.h
 ** File description:
-** All protos and struct about the game informations
+** All protos and struct about the game infos
 */
 
 #ifndef GAME_H
     #define GAME_H
     #include <SFML/Graphics.h>
+    #include "button.h"
 
 typedef struct game_s {
     sfRenderWindow *window;
     sfVideoMode *window_size_list;
     sfVector2f scaling;
+    sfEvent event;
     int actual_window_size;
     unsigned int *framerate_list;
     int actual_framerate;
@@ -23,6 +25,8 @@ typedef struct game_s {
 } game_t;
 
 game_t *init_game_struct(void);
+void free_game(game_t *game_info);
+
 int change_size_of_window(game_t *game_info, int add);
 int init_window(game_t *game_info);
 
