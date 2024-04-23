3e55d643b0b5d3ae34b3ad15d78a5439ef914a4c
Azurioh <alan.cunin@epitech.eu>
2024-04-23 19:24:07 +0200

    (+) Add function to free the button struct File added: src/events/free_buttons.c

---
+++ src/free/free_buttons.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2024
+** free_button
+** File description:
+** Function to free a button_t struct
+*/
+
+#include "../../include/myrpg.h"
+
+void free_button(button_t *button)
+{
+    if (!button) {
+        return;
+    }
+    free(button->button_name);
+    sfSprite_destroy(button->image_sprite);
+    free(button);
+    return;
+}
+
+void free_all_buttons(button_t **buttons)
+{
+    if (!buttons) {
+        return;
+    }
+    for (int i = 0; buttons[i]; i++) {
+        free_button(buttons[i]);
+    }
+}
