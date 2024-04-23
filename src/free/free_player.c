4488afb22dceeaa27aaae62ee682ca37b8b3f92d
Azurioh <alan.cunin@epitech.eu>
2024-04-23 19:25:02 +0200

    (+) Add function to free the player struct File added: src/events/free_player.c

---
+++ src/free/free_player.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** free_player.c
+** File description:
+** Function to free the player struct
+*/
+
+#include "../../include/myrpg.h"
+
+void free_player(player_t *player)
+{
+    sfSprite_destroy(player->interface->sprite);
+    sfClock_destroy(player->interface->clock);
+    free(player->interface);
+    for (int i = 0; i < 3; i++) {
+        free(player->skills[i]->name);
+        free(player->skills[i]);
+    }
+    free(player->skills);
+}
