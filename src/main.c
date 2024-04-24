31716be50278af0e643f1fa4734362b5c18e1283
Mambokara <corentin.bergaentzle@epitech.eu>
2024-04-24 10:13:08 +0200

    [+] added the main

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** main.c
+** File description:
+** Main function to start the program
+*/
+
+#include "../include/myrpg.h"
+
+int main(void)
+{
+    return start_game();
+}
