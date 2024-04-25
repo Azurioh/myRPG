a2c272d0e74f36a35c1ac9f2949eb1249cdb5cac
Mambokara <corentin.bergaentzle@epitech.eu>
2024-04-25 14:08:25 +0200

    [+] added the player functions

---
+++ src/player/animate_player.c
@@ -0,0 +1,6 @@
+/*
+** EPITECH PROJECT, 2024
+** animate_player
+** File description:
+** Function to animate the sprite during his movement
+*/
---
+++ src/player/update_life.c
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2024
+** update_life.c
+** File description:
+** Functions to update the life of the player
+*/
+
+#include "../../include/myrpg.h"
+
+int increase_player_life(player_t *player, unsigned int amount)
+{
+    if (!player) {
+        return -1;
+    }
+    if (player->life == 100) {
+        return 0;
+    }
+    player->life += amount;
+    if (player->life > 100) {
+        player->life -= (player->life % 100);
+    }
+    return 0;
+}
+
+int decrease_player_life(player_t *player, unsigned int amount)
+{
+    if (!player) {
+        return -1;
+    }
+    if (player->life == 0) {
+        return 0;
+    }
+    player->life -= amount;
+    if (player->life <= 0) {
+        player->is_alive = 0;
+        player->nb_dead++;
+    }
+    return 0;
+}
