03ef520a9d9cb9cb24e03ef7ff4e5ee26b84e504
Azurioh <alan.cunin@epitech.eu>
2024-04-25 22:42:17 +0200

    (~) Update the function to init the myrpg struct File modified: src/loop/init_program.c

--- src/loop/init_program.c
+++ src/loop/init_program.c
@@ -7,6 +7,18 @@
 
 #include "../../include/myrpg.h"
 
+static settings_t *init_settings(game_t *game_info)
+{
+    settings_t *settings = malloc(sizeof(settings_t));
+
+    settings->scaling = 1;
+    settings->window = game_info->window;
+    settings->show_settings_menu = 0;
+    settings->visual_elements = NULL;
+    load_settings_menu(settings);
+    return settings;
+}
+
 static myrpg_t *init_myrpg(void)
 {
     myrpg_t *myrpg = malloc(sizeof(myrpg_t));
@@ -16,7 +28,8 @@ static myrpg_t *init_myrpg(void)
     }
     myrpg->player = init_player();
     myrpg->game_info = init_game_struct();
-    myrpg->list_of_events = NULL;
+    myrpg->settings = init_settings(GAME_INFO);
+    myrpg->list_of_events = init_events();
     myrpg->background = NULL;
     myrpg->buttons = NULL;
     return myrpg;
