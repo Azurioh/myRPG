6b0a7f614b043b09e69ab94ad2c5140231d8d323
Mambokara <corentin.bergaentzle@epitech.eu>
2024-04-25 14:07:04 +0200

    [+] added the free functions

---
+++ src/free/free_myrpg.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** free_myrpg
+** File description:
+** Free the myrpg struct
+*/
+
+#include "../../include/myrpg.h"
+
+void free_myrpg(myrpg_t *myrpg)
+{
+    if (!myrpg) {
+        return;
+    }
+    free_player(myrpg->player);
+    free_game(myrpg->game_info);
+    free_events(myrpg->list_of_events);
+    sfSprite_destroy(myrpg->background);
+    free_all_buttons(myrpg->buttons);
+    free(myrpg);
+}
