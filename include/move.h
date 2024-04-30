0e03e6de73ececaa267c70d0e18af10cabbac3df
Mambokara <corentin.bergaentzle@epitech.eu>
2024-04-30 22:46:58 +0200

    [+] added some include

--- include/move.h
+++ include/move.h
@@ -8,13 +8,15 @@
 #ifndef MOVE_H
     #define MOVE_H
     #include "game.h"
+    #include "hud.h"
     #define MOVE_UP sfKeyUp
     #define MOVE_DOWN sfKeyDown
     #define MOVE_LEFT sfKeyLeft
     #define MOVE_RIGHT sfKeyRight
 
-void move_down_view(game_t *game_info);
-void move_up_view(game_t *game_info);
-void move_left_view(game_t *game_info);
-void move_right_view(game_t *game_info);
+void move_down_view(game_t *game_info, hud_t *hud);
+void move_up_view(game_t *game_info, hud_t *hud);
+void move_left_view(game_t *game_info, hud_t *hud);
+void move_right_view(game_t *game_info, hud_t *hud);
+void move_hud(hud_t *hud, int x, int y);
 #endif
