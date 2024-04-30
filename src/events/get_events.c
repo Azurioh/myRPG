f433ee5401ac07386f753c52e577619b46a4f17c
Mambokara <corentin.bergaentzle@epitech.eu>
2024-04-30 22:47:21 +0200

    [+] added a parameter to some functions

--- src/events/get_events.c
+++ src/events/get_events.c
@@ -6,29 +6,30 @@
 */
 
 #include "../../include/move.h"
+#include "../../include/hud.h"
 #include <SFML/Window/Event.h>
 #include <SFML/Window/Keyboard.h>
 
-void make_move(game_t *game_info)
+void make_move(game_t *game_info, hud_t *hud)
 {
     if (game_info->event.type == sfEvtKeyPressed &&
         game_info->event.key.code == MOVE_DOWN)
-        move_down_view(game_info);
+        move_down_view(game_info, hud);
     if (game_info->event.type == sfEvtKeyPressed &&
         game_info->event.key.code == MOVE_UP)
-        move_up_view(game_info);
+        move_up_view(game_info, hud);
     if (game_info->event.type == sfEvtKeyPressed &&
         game_info->event.key.code == MOVE_RIGHT)
-        move_right_view(game_info);
+        move_right_view(game_info, hud);
     if (game_info->event.type == sfEvtKeyPressed &&
         game_info->event.key.code == MOVE_LEFT)
-        move_left_view(game_info);
+        move_left_view(game_info, hud);
 }
 
-int check_events(game_t *game_info)
+int check_events(game_t *game_info, hud_t *hud)
 {
     if (game_info->event.type == sfEvtClosed)
         return -1;
-    make_move(game_info);
+    make_move(game_info, hud);
     return 0;
 }
