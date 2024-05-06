a7b2d1c54ea774fe980c58153a048bc79afd3d6b
Azurioh <alan.cunin@epitech.eu>
2024-05-06 22:24:43 +0200

    (+) Create function to init the game struct File added: src/game/init.c

---
+++ src/game/init.c
@@ -0,0 +1,59 @@
+/*
+** EPITECH PROJECT, 2024
+** init.c
+** File description:
+** Function to init the game struct
+*/
+
+#include "../../include/myrpg.h"
+
+static sfSprite *init_map(void)
+{
+    sfTexture *tmp_texture = sfTexture_createFromFile(MAP_PATH, NULL);
+    sfSprite *tmp_sprite = sfSprite_create();
+
+    sfSprite_setTexture(tmp_sprite, tmp_texture, sfFalse);
+    return tmp_sprite;
+}
+
+static sfView *init_view(game_t *game_info)
+{
+    sfView *tmp_view = sfView_create();
+    sfFloatRect pos_view = sfSprite_getGlobalBounds(game_info->map);
+    sfVector2f center_map = {pos_view.height / 2, pos_view.width / 2};
+
+    sfView_setCenter(tmp_view, (sfVector2f){center_map.y, center_map.x});
+    sfView_setSize(tmp_view, (sfVector2f){1920, 1080});
+    sfView_zoom(tmp_view, 0.65);
+    return tmp_view;
+}
+
+static sfSprite *create_player(game_t *game_info)
+{
+    sfTexture *player = sfTexture_createFromFile(PLAYER_PATH, NULL);
+    sfSprite *sprite_player = sfSprite_create();
+
+    sfSprite_setTexture(sprite_player, player, sfFalse);
+    sfSprite_setPosition(sprite_player, sfView_getCenter(game_info->map_view));
+    return sprite_player;
+}
+
+sfImage *init_undermap(void)
+{
+    sfImage *undermap = sfImage_createFromFile(UNDERMAP_PATH);
+
+    return undermap;
+}
+
+game_t *init_game_struct(void)
+{
+    game_t *game_info = malloc(sizeof(game_t));
+
+    if (!game_info)
+        return NULL;
+    game_info->map = init_map();
+    game_info->undermap = init_undermap();
+    game_info->map_view = init_view(game_info);
+    game_info->player = create_player(game_info);
+    return game_info;
+}
