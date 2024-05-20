120d58667ee687037327b3755d218c3d3fc520ea
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-20 13:56:06 +0200

    [+] stock the pos of the player

---
+++ include/portal.h
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2024
+** myRPG
+** File description:
+** portal
+*/
+
+#ifndef INCLUDED_PORTAL_H
+    #define INCLUDED_PORTAL_H
+    #include <SFML/Graphics.h>
+
+typedef struct portal_pos_s {
+    sfVector2f pos;
+    sfVector2f pos_tp;
+    sfRectangleShape *rect;
+    int is_teleportor;
+    int id;
+} portal_pos_t;
+
+typedef struct portal_s {
+    portal_pos_t **portal_pos;
+    int nbr_portal;
+} portal_t;
+
+
+portal_t *portal_map(void);
+#endif
