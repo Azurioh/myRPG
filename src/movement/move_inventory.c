607419d54afcf39b2e09f6f3e72d161efdaffd9d
milinmatteo <matteo.milin@epitech.eu>
2024-05-20 17:28:17 +0200

    [+] Add the movement of item in inventory when the player move

--- src/movement/move_inventory.c
+++ src/movement/move_inventory.c
@@ -12,4 +12,9 @@
 void move_inventory(inventory_t *inventory, sfVector2f offset)
 {
     sfSprite_move(inventory->image->sprite, offset);
+    for (int i = 0; inventory->items[i] != NULL; i++) {
+        sfSprite_move(inventory->items[i]->sprite, offset);
+        inventory->items[i]->position.x += offset.x;
+        inventory->items[i]->position.y += offset.y;
+    }
 }
