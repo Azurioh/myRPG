f1e66526cca4d7b1b9f9614c0a4419ad52690185
Azurioh <alan.cunin@epitech.eu>
2024-05-20 14:54:34 +0200

    (~) Edit the init window struct to detect if the window must be in fullscreen or windowed File modified: src/settings/init_window.c

--- src/settings/init_window.c
+++ src/settings/init_window.c
@@ -7,7 +7,27 @@
 
 #include "../../include/myrpg.h"
 
-int init_window(settings_t *settings)
+int init_window_fullscreen(settings_t *settings)
+{
+    sfRenderWindow *window;
+    sfVideoMode window_size;
+
+    if (!settings || !settings->window_size_list) {
+        return -1;
+    }
+    window_size = settings->window_size_list[1];
+    if (settings->window) {
+        sfRenderWindow_destroy(settings->window);
+    }
+    window = sfRenderWindow_create(window_size, "MyRPG",
+        sfClose | sfFullscreen | sfDefaultStyle, NULL);
+    sfRenderWindow_setFramerateLimit(window,
+        settings->framerate_list[settings->actual_framerate]);
+    settings->window = window;
+    return 0;
+}
+
+int init_window_windowed(settings_t *settings)
 {
     sfRenderWindow *window;
     sfVideoMode window_size;
@@ -16,13 +36,22 @@ int init_window(settings_t *settings)
         return -1;
     }
     window_size = settings->window_size_list[settings->actual_window_size];
+    if (settings->window) {
+        sfRenderWindow_destroy(settings->window);
+    }
     window = sfRenderWindow_create(window_size, "MyRPG",
         sfClose | sfDefaultStyle, NULL);
     sfRenderWindow_setFramerateLimit(window,
         settings->framerate_list[settings->actual_framerate]);
-    if (settings->window) {
-        sfRenderWindow_destroy(settings->window);
-    }
     settings->window = window;
     return 0;
+}
+
+int init_window(settings_t *settings, int fullscreen)
+{
+    if (fullscreen == 1) {
+        return init_window_fullscreen(settings);
+    } else {
+        return init_window_windowed(settings);
+    }
 }
