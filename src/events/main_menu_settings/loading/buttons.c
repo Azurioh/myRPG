79232c02af644074f72776e3866849fa6177e164
Azurioh <alan.cunin@epitech.eu>
2024-05-20 18:31:38 +0200

    (+) Update the UI of the settings menu and add a button to edit the controls Files modified: src/events/main_menu_settings/loading/buttons.c src/events/main_menu_settings/loading/text.c

--- src/events/main_menu_settings/loading/buttons.c
+++ src/events/main_menu_settings/loading/buttons.c
@@ -33,42 +33,42 @@ static void manage_settings_button_action(button_t *button, void *args)
 static void init_left_choice_button(button_t **buttons, settings_t *settings)
 {
     buttons[0] = init_button("decrease_fps",
-        (sfVector2f){630 * settings->scaling, 350 * settings->scaling},
-        "assets/left-choice-button.png", &manage_settings_button_action);
+        (sfVector2f){1150 * settings->scaling, 240 * settings->scaling},
+        "assets/less.png", &manage_settings_button_action);
     buttons[2] = init_button("decrease_resolution",
-        (sfVector2f){1080 * settings->scaling, 350 * settings->scaling},
-        "assets/left-choice-button.png", &manage_settings_button_action);
+        (sfVector2f){1150 * settings->scaling, 360 * settings->scaling},
+        "assets/less.png", &manage_settings_button_action);
     buttons[4] = init_button("decrease_music",
-        (sfVector2f){630 * settings->scaling, 700 * settings->scaling},
-        "assets/left-choice-button.png", &manage_settings_button_action);
+        (sfVector2f){1150 * settings->scaling, 480 * settings->scaling},
+        "assets/less.png", &manage_settings_button_action);
     buttons[6] = init_button("decrease_sound",
-        (sfVector2f){1070 * settings->scaling, 700 * settings->scaling},
-        "assets/left-choice-button.png", &manage_settings_button_action);
+        (sfVector2f){1150 * settings->scaling, 600 * settings->scaling},
+        "assets/less.png", &manage_settings_button_action);
     for (int i = 0; i <= 6; i += 2) {
         sfSprite_setScale(buttons[i]->image_sprite,
-            (sfVector2f){settings->scaling * 2, settings->scaling * 2});
-        buttons[i]->initial_scaling = settings->scaling * 2;
+            (sfVector2f){settings->scaling / 1.8, settings->scaling / 1.8});
+        buttons[i]->initial_scaling = settings->scaling / 1.8;
     }
 }
 
 static void init_right_choice_button(button_t **buttons, settings_t *settings)
 {
     buttons[1] = init_button("increase_fps",
-        (sfVector2f){750 * settings->scaling, 350 * settings->scaling},
-        "assets/right-choice-button.png", &manage_settings_button_action);
+        (sfVector2f){1250 * settings->scaling, 240 * settings->scaling},
+        "assets/more.png", &manage_settings_button_action);
     buttons[3] = init_button("increase_resolution",
-        (sfVector2f){1320 * settings->scaling, 350 * settings->scaling},
-        "assets/right-choice-button.png", &manage_settings_button_action);
+        (sfVector2f){1250 * settings->scaling, 360 * settings->scaling},
+        "assets/more.png", &manage_settings_button_action);
     buttons[5] = init_button("increase_music",
-        (sfVector2f){750 * settings->scaling, 700 * settings->scaling},
-        "assets/right-choice-button.png", &manage_settings_button_action);
+        (sfVector2f){1250 * settings->scaling, 480 * settings->scaling},
+        "assets/more.png", &manage_settings_button_action);
     buttons[7] = init_button("increase_sound",
-        (sfVector2f){1170 * settings->scaling, 700 * settings->scaling},
-        "assets/right-choice-button.png", &manage_settings_button_action);
+        (sfVector2f){1250 * settings->scaling, 600 * settings->scaling},
+        "assets/more.png", &manage_settings_button_action);
     for (int i = 1; i <= 7; i += 2) {
         sfSprite_setScale(buttons[i]->image_sprite,
-            (sfVector2f){settings->scaling * 2, settings->scaling * 2});
-        buttons[i]->initial_scaling = settings->scaling * 2;
+            (sfVector2f){settings->scaling / 1.8, settings->scaling / 1.8});
+        buttons[i]->initial_scaling = settings->scaling / 1.8;
     }
 }
 
@@ -78,21 +78,32 @@ static void close_settings(button_t *button, void *args)
     unload_settings(args);
 }
 
+static button_t *init_control_button(settings_t *settings)
+{
+    button_t *button = init_button("controls",
+        (sfVector2f){960 * settings->scaling, 730 * settings->scaling},
+        "assets/controls_button.png", NULL);
+
+    button->initial_scaling = settings->scaling;
+    return button;
+}
+
 button_t **load_settings_buttons(settings_t *settings)
 {
-    button_t **buttons = malloc(sizeof(button_t *) * 10);
+    button_t **buttons = malloc(sizeof(button_t *) * 11);
 
     if (!buttons) {
         return NULL;
     }
     init_left_choice_button(buttons, settings);
     init_right_choice_button(buttons, settings);
-    buttons[8] = init_button("quit",
-        (sfVector2f){950 * settings->scaling, 800 * settings->scaling},
+    buttons[8] = init_control_button(settings);
+    buttons[9] = init_button("quit",
+        (sfVector2f){960 * settings->scaling, 860 * settings->scaling},
         "assets/quit-button.png", &close_settings);
-    buttons[8]->initial_scaling = settings->scaling / 2;
-    sfSprite_setScale(buttons[8]->image_sprite,
-        (sfVector2f){settings->scaling / 2, settings->scaling / 2});
-    buttons[9] = NULL;
+    buttons[9]->initial_scaling = settings->scaling / 1.5;
+    sfSprite_setScale(buttons[9]->image_sprite,
+        (sfVector2f){settings->scaling / 1.5, settings->scaling / 1.5});
+    buttons[10] = NULL;
     return buttons;
 }
--- src/events/main_menu_settings/loading/text.c
+++ src/events/main_menu_settings/loading/text.c
@@ -10,38 +10,37 @@
 void load_static_settings_texts(sfText **texts, settings_t *settings)
 {
     float scaling = settings->scaling;
+    sfColor color = sfColor_fromRGBA(200, 200, 200, 255);
 
-    texts[0] = init_text("FPS", (sfVector2f){ 692 * settings->scaling,
-        280 * settings->scaling}, 40, sfBlack);
-    texts[1] = init_text("Resolution", (sfVector2f){ 1210 * settings->scaling,
-        280 * settings->scaling}, 40, sfBlack);
-    texts[2] = init_text("Musique", (sfVector2f){ 692 * settings->scaling,
-        630 * settings->scaling}, 40, sfBlack);
-    texts[3] = init_text("Son", (sfVector2f){ 1120 * settings->scaling,
-        630 * settings->scaling}, 40, sfBlack);
+    texts[0] = init_text("FPS :", (sfVector2f){ 620 * settings->scaling,
+        200 * settings->scaling}, 60, color);
+    texts[1] = init_text("Taille :", (sfVector2f){ 620 *
+        settings->scaling, 320 * settings->scaling}, 60, color);
+    texts[2] = init_text("Son :", (sfVector2f){ 620 * settings->scaling,
+        440 * settings->scaling}, 60, color);
+    texts[3] = init_text("Musique :", (sfVector2f){ 620 * settings->scaling,
+        560 * settings->scaling}, 60, color);
     for (int i = 0; i < 4; i++) {
         sfText_setScale(texts[i], (sfVector2f){scaling, scaling});
+        sfText_setOrigin(texts[i], (sfVector2f){0, 0});
     }
 }
 
 void load_dynamic_settings_texts(sfText **texts, settings_t *settings)
 {
     float scaling = settings->scaling;
-    sfFloatRect rect;
 
-    texts[4] = init_text("60", (sfVector2f){ 690 * settings->scaling,
-        338 * settings->scaling}, 30, sfBlack);
-    texts[5] = init_text("1920x1080", (sfVector2f){ 1208 * settings->scaling,
-        340 * settings->scaling}, 30, sfBlack);
-    texts[6] = init_text("50%", (sfVector2f){ 700 * settings->scaling,
-        690 * settings->scaling}, 30, sfBlack);
-    texts[7] = init_text("50%", (sfVector2f){ 1120 * settings->scaling,
-        690 * settings->scaling}, 30, sfBlack);
+    texts[4] = init_text("60", (sfVector2f){ 800 * settings->scaling,
+        210 * settings->scaling}, 40, sfWhite);
+    texts[5] = init_text("1920x1080", (sfVector2f){ 850 * settings->scaling,
+        330 * settings->scaling}, 40, sfWhite);
+    texts[6] = init_text("50%", (sfVector2f){ 800 * settings->scaling,
+        450 * settings->scaling}, 40, sfWhite);
+    texts[7] = init_text("50%", (sfVector2f){ 920 * settings->scaling,
+        573 * settings->scaling}, 40, sfWhite);
     for (int i = 4; i < 8; i++) {
         sfText_setScale(texts[i], (sfVector2f){scaling * 1.2, scaling * 1.2});
-        rect = sfText_getGlobalBounds(texts[i]);
-        sfText_setOrigin(texts[i],
-            (sfVector2f){(rect.width / 2) / 1.2, (rect.height / 2) / 1.2});
+        sfText_setOrigin(texts[i], (sfVector2f){0, 0});
     }
 }
 
