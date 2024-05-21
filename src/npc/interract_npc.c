24a6ebb8271047820d080bbb1eddab27e7226607
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-21 10:44:41 +0200

    [+] add the detection when we are next to a npc

--- src/npc/interract_npc.c
+++ src/npc/interract_npc.c
@@ -4,8 +4,26 @@
 ** File description:
 ** interract_npc
 */
+#include "../../include/myrpg.h"
+#include "../../include/npc.h"
 
-void spawn_npc()
+int detect_npc(player_t *player, npc_t *npc, myrpg_t *myrpg, int i)
 {
+    player->pos = sfView_getCenter(myrpg->game_info->map_view);
+    if(player->pos.x >= npc->npc_info[i]->pos.x &&
+        player->pos.x <= npc->npc_info[i]->pos.x + 50 &&
+        player->pos.y >= npc->npc_info[i]->pos.y && player->pos.y <=
+        npc->npc_info[i]->pos.y + 20) {
+        return i;
+    }
+    return -1;
+}
 
+void can_speak(myrpg_t *myrpg,  npc_t *npc, int i)
+{
+    for (int i = 0; i != NULL; i++) {
+        if (myrpg->can_interact == 1) {
+            draw_text(npc, detect_npc(myrpg->player, npc, myrpg, i));
+        }
+    }
 }
