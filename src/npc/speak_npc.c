605be7d0f5a9d808e01e634895be911421d6d1ee
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-21 13:14:43 +0200

    [+] create txt for npc

--- src/npc/speak_npc.c
+++ src/npc/speak_npc.c
@@ -4,4 +4,10 @@
 ** File description:
 ** speak_npc
 */
+#include "../../include/myrpg.h"
+#include "../../include/npc.h"
 
+void draw_text(npc_t *npc, int i)
+{
+    create_text(npc->npc_info[i]->text, 10, npc->npc_info[i]->pos, FONT);
+}
