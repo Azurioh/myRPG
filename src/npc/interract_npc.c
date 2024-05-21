913cf18f23b93f25187a57005889101fa8cd872f
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-21 13:33:09 +0200

    [0] Coding-Style

--- src/npc/interract_npc.c
+++ src/npc/interract_npc.c
@@ -10,7 +10,7 @@
 int detect_npc(player_t *player, npc_t *npc, myrpg_t *myrpg, int i)
 {
     player->pos = sfView_getCenter(myrpg->game_info->map_view);
-    if(player->pos.x >= npc->npc_info[i]->pos.x &&
+    if (player->pos.x >= npc->npc_info[i]->pos.x &&
         player->pos.x <= npc->npc_info[i]->pos.x + 50 &&
         player->pos.y >= npc->npc_info[i]->pos.y && player->pos.y <=
         npc->npc_info[i]->pos.y + 20) {
@@ -19,7 +19,7 @@ int detect_npc(player_t *player, npc_t *npc, myrpg_t *myrpg, int i)
     return -1;
 }
 
-void can_speak(myrpg_t *myrpg,  npc_t *npc, int i)
+void can_speak(myrpg_t *myrpg, npc_t *npc, int i)
 {
     for (int i = 0; i != NULL; i++) {
         if (myrpg->can_interact == 1) {
