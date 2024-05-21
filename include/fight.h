1d367dd09cb3f54ccc3f6fb98c2a3536cfbe48ff
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-22 01:38:11 +0200

    [+] added a value in the struct

--- include/fight.h
+++ include/fight.h
@@ -11,6 +11,11 @@
     #include "button.h"
     #include <SFML/System/Vector2.h>
 
+enum turn {
+    TOSKRA = 0,
+    ENEMY,
+};
+
 typedef struct button_attack_s {
     char *button_name;
     sfVector2f position;
@@ -25,9 +30,19 @@ typedef struct button_attack_s {
     char *(*action)(void *args);
 } button_attack_t;
 
+typedef struct enemy_s {
+    int rage_turns;
+    int heal_turns;
+    int did_attack;
+    int rage;
+    int attack_turns;
+} enemy_t;
+
 typedef struct fight_s {
     button_attack_t **buttons;
+    enemy_t *enemy_infos;
     int in_fight;
+    enum turn turn;
     sfVector2f pos;
     int enemy_hp;
     int toskra_hp;
@@ -41,5 +56,7 @@ char *another_attack(void *fight);
 char *drink_hydromel(void *fight);
 char *angry_guy(void *fight);
 int check_attack_collision(button_attack_t *button, sfRenderWindow *window);
+fight_t *enemy_attack(fight_t *fight);
+enemy_t *init_enemy(void);
 
 #endif
