33c4f941e857b02ee7debfccf83fd26c473d163a
milinmatteo <matteo.milin@epitech.eu>
2024-05-22 11:58:56 +0200

    [+] Add -h and --help

--- src/main.c
+++ src/main.c
@@ -7,7 +7,25 @@
 
 #include "../include/myrpg.h"
 
-int main(void)
+int display_help(void)
 {
-    return start_game();
+    printf("Welcome to Toskra's quest !\n");
+    printf("To move use 'ZQSD' key or change key configuration in settings\n");
+    printf("You can use 'E' to open inventory and ESC to open settings\n");
+    printf("The goal of this game is to find your family ");
+    printf("that has been kidnapped\n");
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc >= 2) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+            return display_help();
+        } else {
+            printf("Wrong parameters try: ./my_rpg --help\n");
+            return 84;
+        }
+    } else
+        return start_game();
 }
