e78cd247b6f683c2994718f951aadc1340ac50f1
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-22 11:32:15 +0200

    [+] call the function to check if the player is on a portal

--- src/events/game/event.c
+++ src/events/game/event.c
@@ -27,6 +27,13 @@ static void manage_escape_key_game(myrpg_t *myrpg)
     }
 }
 
+void draw_rectangle(myrpg_t *myrpg)
+{
+    for (int i = 0; i < 2; i++) {
+        sfRenderWindow_drawRectangleShape(myrpg->settings->window, myrpg->portal->portal_pos[i]->rect, NULL);
+    }
+}
+
 static void exec_game_events(void *args)
 {
     myrpg_t *myrpg = args;
@@ -45,7 +52,8 @@ static void exec_game_events(void *args)
         }
         manage_escape_key_game(myrpg);
     }
-}
+    check_portal(myrpg);
+}   
 
 void load_game(void *args)
 {
