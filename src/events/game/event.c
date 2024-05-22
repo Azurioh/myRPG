d7f35433d1bce3e5c49f9ce73005e0e673283861
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-22 12:54:14 +0200

    [0] Coding-style

--- src/events/game/event.c
+++ src/events/game/event.c
@@ -30,7 +30,8 @@ static void manage_escape_key_game(myrpg_t *myrpg)
 void draw_rectangle(myrpg_t *myrpg)
 {
     for (int i = 0; i < 2; i++) {
-        sfRenderWindow_drawRectangleShape(myrpg->settings->window, myrpg->portal->portal_pos[i]->rect, NULL);
+        sfRenderWindow_drawRectangleShape(myrpg->settings->window,
+            myrpg->portal->portal_pos[i]->rect, NULL);
     }
 }
 
@@ -53,7 +54,7 @@ static void exec_game_events(void *args)
         manage_escape_key_game(myrpg);
     }
     check_portal(myrpg);
-}   
+}
 
 void load_game(void *args)
 {
