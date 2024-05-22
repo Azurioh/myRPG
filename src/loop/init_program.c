d8ded37680bff5b34796d0fe59d733c978e9b378
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-22 03:14:39 +0200

    [+] added a the init of the mobs

--- src/loop/init_program.c
+++ src/loop/init_program.c
@@ -6,6 +6,9 @@
 */
 
 #include "../../include/myrpg.h"
+#include <SFML/Graphics/Color.h>
+#include <SFML/Graphics/RectangleShape.h>
+#include <SFML/System/Vector2.h>
 #include <stdio.h>
 
 static myrpg_t *init_myrpg(void)
@@ -32,11 +35,43 @@ static myrpg_t *init_myrpg(void)
     return myrpg;
 }
 
+mobs_t **init_mobs(void)
+{
+    mobs_t **mobs = malloc(sizeof(mobs_t *) * 1);
+
+    mobs[0] = malloc(sizeof(mobs_t));
+    mobs[0]->life = 100;
+    mobs[0]->id = 0;
+    mobs[0]->can_collide = 1;
+    mobs[0]->level = 1;
+    mobs[0]->damages = 20;
+    mobs[0]->xp_loot = 15;
+    mobs[0]->item_loot = 1;
+    mobs[0]->pos = (sfVector2f){4500, 4500};
+    mobs[0]->hitbox = sfRectangleShape_create();
+    sfRectangleShape_setPosition(mobs[0]->hitbox, mobs[0]->pos);
+    sfRectangleShape_setSize(mobs[0]->hitbox, (sfVector2f){100, 100});
+    sfRectangleShape_setFillColor(mobs[0]->hitbox, sfBlack);
+    return mobs;
+}
+
+sfRectangleShape *init_hitbox(game_t *game_info)
+{
+    sfRectangleShape *rect = sfRectangleShape_create();
+
+    (void)game_info;
+    sfRectangleShape_setSize(rect, (sfVector2f){20, 20});
+    sfRectangleShape_setPosition(rect, (sfVector2f){20, 20});
+    return rect;
+}
+
 int start_game(void)
 {
     myrpg_t *myrpg = init_myrpg();
 
+    myrpg->mobs = init_mobs();
     myrpg->fight_infos->in_fight = 0;
+    myrpg->hitbox = init_hitbox(myrpg->game_info);
     if (!myrpg) {
         return 84;
     } else {
