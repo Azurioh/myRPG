013ad48d23f0d8abe020b96a542a2d8e4300bf1f
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-22 11:30:37 +0200

    [0] Fix portal

--- src/Portal/portal.c
+++ src/Portal/portal.c
@@ -16,12 +16,15 @@ void link_portal(portal_t *portal, int src, int dest)
 
 int coord_portal(player_t *player, portal_t *portal, myrpg_t *myrpg, int i)
 {
-    player->pos = sfView_getCenter(myrpg->game_info->map_view);
-    if (player->pos.x >= portal->portal_pos[i]->pos.x &&
-        player->pos.x <= portal->portal_pos[i]->pos.x + 200 &&
-        player->pos.y >= portal->portal_pos[i]->pos.y && player->pos.y <=
-        portal->portal_pos[i]->pos.y + 200) {
-        return i;
+    sfVector2f pos = sfView_getCenter(myrpg->game_info->map_view);
+    if (pos.x >= portal->portal_pos[i]->pos.x &&
+        pos.x <= portal->portal_pos[i]->pos.x + 100 &&
+        pos.y >= portal->portal_pos[i]->pos.y && pos.y <=
+        portal->portal_pos[i]->pos.y + 100) {
+            myrpg->can_interact = 1;
+            return i;
+    } else {
+        myrpg->can_interact = 0;
     }
     return -1;
 }
@@ -29,18 +32,18 @@ int coord_portal(player_t *player, portal_t *portal, myrpg_t *myrpg, int i)
 void teleport(player_t *player, portal_t *portal, int i, myrpg_t *myrpg)
 {
     if (coord_portal(player, portal, myrpg, i) != -1 &&
-    portal->portal_pos[i]->is_teleportor != 0) {
+    portal->portal_pos[i]->is_teleportor == 1 && sfKeyboard_isKeyPressed(myrpg->game_info->keybinds->interact)) {
         player->pos.x = portal->portal_pos[i]->pos_tp.x;
         player->pos.y = portal->portal_pos[i]->pos_tp.y;
-        sfSprite_setPosition(myrpg->game_info->player, player->pos);
+            set_all_after_tp(myrpg, portal->portal_pos[i]->pos_tp);
     }
 }
 
 static portal_pos_t **fill_struct_portal_map(void)
 {
-    portal_pos_t **portal_pos = malloc(sizeof(portal_t *)*3);
+    portal_pos_t **portal_pos = malloc(sizeof(portal_t *) * 3);
     sfVector2f portal_0 = {5000, 5000};
-    sfVector2f portal_1 = {6000, 6000};
+    sfVector2f portal_1 = {4500, 4500};
 
     for (int i = 0; i < 2; i++) {
         portal_pos[i] = malloc(sizeof(portal_pos_t));
@@ -57,19 +60,19 @@ static portal_pos_t **fill_struct_portal_map(void)
 static void fill_struct_portal_isteleportor_map(portal_t *portal)
 {
     portal->portal_pos[0]->is_teleportor = 1;
-    portal->portal_pos[1]->is_teleportor = 0;
+    portal->portal_pos[1]->is_teleportor = 1;
 }
 
-static void create_rectangle_colision_map(portal_t *portal)
+static void create_rectangle_collision_map(portal_t *portal)
 {
-    sfVector2f scale = {200, 200};
+    sfVector2f scale = {100, 100};
 
     for (int i = 0; i < 2; i++) {
         portal->portal_pos[i]->rect = sfRectangleShape_create();
         sfRectangleShape_setSize(portal->portal_pos[i]->rect, scale);
         sfRectangleShape_setPosition(portal->portal_pos[i]->rect,
             portal->portal_pos[i]->pos);
-        sfRectangleShape_setFillColor(portal->portal_pos[i]->rect, sfWhite);
+        sfRectangleShape_setFillColor(portal->portal_pos[i]->rect, sfBlack);
     }
 }
 
@@ -79,7 +82,7 @@ portal_t *portal_map(void)
 
     portal->portal_pos = fill_struct_portal_map();
     fill_struct_portal_isteleportor_map(portal);
-    create_rectangle_colision_map(portal);
+    create_rectangle_collision_map(portal);
     link_portal(portal, 0, 1);
     return portal;
 }
