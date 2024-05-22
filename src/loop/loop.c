db499598b5d3f89b7de95baa67d4db76c5efc89e
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-22 03:14:49 +0200

    [+] added a the check of the collisions

--- src/loop/loop.c
+++ src/loop/loop.c
@@ -6,8 +6,24 @@
 */
 
 #include "../../include/myrpg.h"
+#include <SFML/Config.h>
+#include <SFML/Graphics/Rect.h>
+#include <SFML/Graphics/RectangleShape.h>
 #include <SFML/Window/Keyboard.h>
 #include <stdio.h>
+
+void check_coll_enemy(myrpg_t *myrpg)
+{
+    sfFloatRect rect_enemy = sfRectangleShape_getGlobalBounds(
+    myrpg->mobs[0]->hitbox);
+    sfFloatRect rect_player = sfRectangleShape_getGlobalBounds(myrpg->hitbox);
+
+    if (sfFloatRect_intersects(&rect_player, &rect_enemy, NULL) == sfTrue &&
+    myrpg->mobs[0]->can_collide == 1) {
+        fight(myrpg);
+        myrpg->mobs[0]->can_collide = 0;
+    }
+}
 
 void game_loop(myrpg_t *myrpg)
 {
@@ -19,9 +35,7 @@ void game_loop(myrpg_t *myrpg)
             myrpg->game_open = 0;
         }
         EVENTS->event_function(myrpg);
-        if (sfKeyboard_isKeyPressed(sfKeyA)) {
-            fight(myrpg);
-        }
+        check_coll_enemy(myrpg);
     }
     sfRenderWindow_clear(settings->window, sfBlack);
     EVENTS->display_function(myrpg);
