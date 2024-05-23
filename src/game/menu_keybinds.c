29d75b8304ddd56bc43b32d140e66a654fbe32e8
milinmatteo <matteo.milin@epitech.eu>
2024-05-24 01:02:19 +0200

    [+] Add "courir" and "competences" in the settings display

--- src/game/menu_keybinds.c
+++ src/game/menu_keybinds.c
@@ -21,26 +21,34 @@ static void load_static_keybind_texts(sfText **texts, sfVector2f resize)
         (sfVector2f){ resize.x + 90, resize.y + 310 }, 30, color);
     texts[3] = init_text("Droite :",
         (sfVector2f){ resize.x + 90, resize.y + 350 }, 30, color);
-    texts[4] = init_text("Inventaire :",
+    texts[4] = init_text("Courir :",
         (sfVector2f){ resize.x + 90, resize.y + 390 }, 30, color);
-    texts[5] = init_text("Interagir :",
+    texts[5] = init_text("Inventaire :",
         (sfVector2f){ resize.x + 90, resize.y + 430 }, 30, color);
+    texts[6] = init_text("Interragir :",
+        (sfVector2f){ resize.x + 90, resize.y + 470 }, 30, color);
+    texts[7] = init_text("Competences :",
+        (sfVector2f){ resize.x + 90, resize.y + 510 }, 30, color);
 }
 
 static void load_dynamic_keybind_texts(sfText **texts, sfVector2f resize)
 {
-    texts[6] = init_text("",
-        (sfVector2f){ resize.x + 235, resize.y + 230 }, 30, sfWhite);
-    texts[7] = init_text("",
-        (sfVector2f){ resize.x + 230, resize.y + 270 }, 30, sfWhite);
     texts[8] = init_text("",
-        (sfVector2f){ resize.x + 220, resize.y + 310 }, 30, sfWhite);
+        (sfVector2f){ resize.x + 235, resize.y + 230 }, 30, sfWhite);
     texts[9] = init_text("",
-        (sfVector2f){ resize.x + 205, resize.y + 350 }, 30, sfWhite);
+        (sfVector2f){ resize.x + 230, resize.y + 270 }, 30, sfWhite);
     texts[10] = init_text("",
-        (sfVector2f){ resize.x + 260, resize.y + 390 }, 30, sfWhite);
+        (sfVector2f){ resize.x + 220, resize.y + 310 }, 30, sfWhite);
     texts[11] = init_text("",
-        (sfVector2f){ resize.x + 224, resize.y + 430 }, 30, sfWhite);
+        (sfVector2f){ resize.x + 205, resize.y + 350 }, 30, sfWhite);
+    texts[12] = init_text("",
+        (sfVector2f){ resize.x + 205, resize.y + 390 }, 30, sfWhite);
+    texts[13] = init_text("",
+        (sfVector2f){ resize.x + 260, resize.y + 430 }, 30, sfWhite);
+    texts[14] = init_text("",
+        (sfVector2f){ resize.x + 260, resize.y + 470 }, 30, sfWhite);
+    texts[15] = init_text("",
+        (sfVector2f){ resize.x + 290, resize.y + 510 }, 30, sfWhite);
 }
 
 void update_dynamic_texts(game_t *game_info, sfText **texts)
@@ -49,28 +57,32 @@ void update_dynamic_texts(game_t *game_info, sfText **texts)
 
     key[0] = game_info->keybinds->move_up + 'A';
     key[1] = '\0';
-    sfText_setString(texts[6], key);
-    key[0] = game_info->keybinds->move_down + 'A';
-    sfText_setString(texts[7], key);
-    key[0] = game_info->keybinds->move_left + 'A';
     sfText_setString(texts[8], key);
-    key[0] = game_info->keybinds->move_right + 'A';
+    key[0] = game_info->keybinds->move_down + 'A';
     sfText_setString(texts[9], key);
-    key[0] = game_info->keybinds->open_inventory + 'A';
+    key[0] = game_info->keybinds->move_left + 'A';
     sfText_setString(texts[10], key);
-    key[0] = game_info->keybinds->interact + 'A';
+    key[0] = game_info->keybinds->move_right + 'A';
     sfText_setString(texts[11], key);
+    key[0] = game_info->keybinds->sprint + 'A';
+    sfText_setString(texts[12], key);
+    key[0] = game_info->keybinds->open_inventory + 'A';
+    sfText_setString(texts[13], key);
+    key[0] = game_info->keybinds->interact + 'A';
+    sfText_setString(texts[14], key);
+    key[0] = game_info->keybinds->skills + 'A';
+    sfText_setString(texts[15], key);
     free(key);
 }
 
 sfText **load_texts_elements(game_t *game_info, sfVector2f resize)
 {
-    sfText **texts = malloc(sizeof(sfText *) * 13);
+    sfText **texts = malloc(sizeof(sfText *) * 17);
 
     load_static_keybind_texts(texts, resize);
     load_dynamic_keybind_texts(texts, resize);
-    texts[12] = NULL;
-    for (int i = 0; i < 12; i++) {
+    texts[16] = NULL;
+    for (int i = 0; i < 16; i++) {
         sfText_setOrigin(texts[i], (sfVector2f){0, 0});
     }
     update_dynamic_texts(game_info, texts);
