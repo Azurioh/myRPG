97aef1f01df9afb5ee251df0e357d8b83f7b2ad7
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-23 20:32:45 +0200

    [0] FIX function to display npc & text of each npc when F is pressed

--- src/npc/init_npc.c
+++ src/npc/init_npc.c
@@ -13,8 +13,9 @@
 sfSprite *init_npc_sprite(void)
 {
     sfSprite *sprite = sfSprite_create();
-    sfTexture *texture = sfTexture_createFromFile(PLAYER_PATH, NULL);
+    sfTexture *texture = sfTexture_createFromFile(NPC, NULL);
 
+    sfSprite_setScale(sprite, (sfVector2f){0.7, 0.7});
     sfSprite_setTexture(sprite, texture, sfFalse);
     return sprite;
 }
@@ -23,7 +24,7 @@ static void create_npc_collision_npc(npc_t *npc)
 {
     sfVector2f scale = {100, 100};
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 3; i++) {
         npc->npc_info[i]->rect = sfRectangleShape_create();
         sfRectangleShape_setSize(npc->npc_info[i]->rect, scale);
         sfRectangleShape_setPosition(npc->npc_info[i]->rect,
@@ -34,20 +35,18 @@ static void create_npc_collision_npc(npc_t *npc)
 
 static npc_info_t **fill_struct_npc(void)
 {
-    npc_info_t **npc_info = malloc(sizeof(npc_info_t *) * 6);
+    npc_info_t **npc_info = malloc(sizeof(npc_info_t *) * 4);
 
     if (!npc_info)
         return NULL;
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 3; i++) {
         npc_info[i] = malloc(sizeof(npc_info_t));
     }
     npc_info[0]->pos = (sfVector2f) {5000, 5000};
     npc_info[1]->pos = (sfVector2f) {5200, 5200};
     npc_info[2]->pos = (sfVector2f) {5400, 5400};
-    npc_info[3]->pos = (sfVector2f) {5600, 5600};
-    npc_info[4]->pos = (sfVector2f) {5800, 5800};
-    npc_info[5] = NULL;
-    for (int i = 0; i < 5; i++) {
+    npc_info[3] = NULL;
+    for (int i = 0; i < 3; i++) {
         npc_info[i]->id = i;
     }
     return npc_info;
@@ -60,7 +59,10 @@ npc_t *init_npc(void)
     npc->npc_info = fill_struct_npc();
     for (int i = 0; npc->npc_info[i] != NULL; i++) {
         npc->npc_info[i]->sprite = init_npc_sprite();
+        sfSprite_setPosition(npc->npc_info[i]->sprite, npc->npc_info[i]->pos);
     }
+    stock_text_in_npc(npc);
+    init_text_for_npc(npc);
     create_npc_collision_npc(npc);
     return npc;
 }
