ceee3092adec8af660695f043ec3820ff0a0c431
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-23 09:28:37 +0200

    Revert "[+] change map"
    This reverts commit 144fbf62cd20b18d9a9b39fd901670235d6357f9.

--- assets/!$WV_Doors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="!$WV_Doors" tilewidth="32" tileheight="32" tilecount="234" columns="13">
- <image source="../../../../../Downloads/craftpix-net-461482-winter-house-top-down-pixel-art/RPG Maker MV and MZ/!$WV_Doors.png" width="432" height="576"/>
-</tileset>
--- assets/Winter_objects_source.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Winter_objects_source" tilewidth="32" tileheight="32" tilecount="550" columns="22">
- <image source="../../../../../Downloads/craftpix-net-232567-top-down-winter-objects-pixel-art/PNG/Winter_objects_source.png" width="720" height="816"/>
-</tileset>
