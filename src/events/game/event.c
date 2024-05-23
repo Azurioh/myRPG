f9e75fbc7b98129965cf397df6c3a16503b1be62
Azurioh <alan.cunin@epitech.eu>
2024-05-23 22:44:36 +0200

    (~) Fix loading quest save system

--- src/events/game/event.c
+++ src/events/game/event.c
@@ -36,7 +36,7 @@ static void open_skills(myrpg_t *myrpg)
     if (EVENTS->event.type != sfEvtKeyPressed)
         return;
     if (EVENTS->event.key.code == GAME_INFO->keybinds->skills
-        && GAME_INFO->show_menu != 1 && QUESTS->quests[0]->is_validate) {
+        && GAME_INFO->show_menu != 1 && QUESTS->quests[0]->is_validate == true) {
         if (GAME_INFO->show_menu == 2) {
             free(GAME_INFO->game_menu);
             GAME_INFO->game_menu = init_game_menu(GAME_INFO, SETTINGS);
