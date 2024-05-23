bb4ee2c023e86a76120d80b38a118f481938fe67
Azurioh <alan.cunin@epitech.eu>
2024-05-23 21:44:01 +0200

    (+) Add keybinds in the init keybinds function

--- src/game/init.c
+++ src/game/init.c
@@ -71,9 +71,10 @@ keybinds_t *init_keybinds(void)
     keybinds->move_down = sfKeyS;
     keybinds->move_left = sfKeyQ;
     keybinds->move_right = sfKeyD;
+    keybinds->sprint = sfKeyR;
     keybinds->open_inventory = sfKeyE;
     keybinds->interact = sfKeyF;
-    keybinds->skills = sfKeyO;
+    keybinds->skills = sfKeyT;
     keybinds->editing_keybinds = sfFalse;
     keybinds->actions = NULL;
     keybinds->keybind_edit = NULL;
