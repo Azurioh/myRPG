7f21f027d69ce88132cf62b6e31dacf7b761df64
milinmatteo <matteo.milin@epitech.eu>
2024-05-25 15:40:51 +0200

    [+] Add enemies to the right side of the map

--- include/mobs.h
+++ include/mobs.h
@@ -35,3 +35,6 @@ typedef struct mobs_s {
 int **init_loot(void);
 mobs_t *create_sprite(mobs_t *mobs);
 mobs_t *init_lvl_one(mobs_t **mobs, int i, sfVector2f pos);
+mobs_t *init_lvl_two(mobs_t **mobs, int i, sfVector2f pos);
+mobs_t *init_lvl_three(mobs_t **mobs, int i, sfVector2f pos);
+mobs_t *init_lvl_four(mobs_t **mobs, int i, sfVector2f pos);
