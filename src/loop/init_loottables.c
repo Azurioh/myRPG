e2c1e98353ccbde6e395d0399c0a1d3b809fbdb1
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-25 17:01:12 +0200

    [+] changed the loot tables

--- src/loop/init_loottables.c
+++ src/loop/init_loottables.c
@@ -5,20 +5,18 @@
 ** init the lootables
 */
 
-int *init_common_table(int *table)
+static int *init_heal_table(int *table)
 {
-    table[0] = 2;
-    table[1] = 3;
-    table[2] = 8;
-    table[3] = 12;
-    table[4] = 16;
-    table[5] = 20;
+    table[0] = 1;
+    table[1] = 2;
+    table[2] = 0;
+    table[3] = 3;
     return table;
 }
 
-int *init_rare_table(int *table)
+static int *init_common_table(int *table)
 {
-    table[0] = 1;
+    table[0] = 4;
     table[1] = 9;
     table[2] = 13;
     table[3] = 17;
@@ -26,7 +24,7 @@ int *init_rare_table(int *table)
     return table;
 }
 
-int *init_epic_table(int *table)
+static int *init_rare_table(int *table)
 {
     table[0] = 5;
     table[1] = 10;
@@ -36,22 +34,32 @@ int *init_epic_table(int *table)
     return table;
 }
 
-int *init_legendary_table(int *table)
+static int *init_epic_table(int *table)
 {
-    table[0] = 0;
-    table[1] = 7;
-    table[2] = 11;
-    table[3] = 15;
-    table[4] = 19;
-    table[5] = 23;
+    table[0] = 6;
+    table[1] = 11;
+    table[2] = 15;
+    table[3] = 19;
+    table[4] = 23;
+    return table;
+}
+
+static int *init_legendary_table(int *table)
+{
+    table[0] = 7;
+    table[1] = 12;
+    table[2] = 16;
+    table[3] = 20;
+    table[4] = 24;
     return table;
 }
 
 int **init_all_lootables(int **table)
 {
-    table[0] = init_common_table(table[0]);
-    table[1] = init_rare_table(table[1]);
-    table[2] = init_epic_table(table[2]);
-    table[3] = init_legendary_table(table[3]);
+    table[0] = init_heal_table(table[0]);
+    table[1] = init_common_table(table[1]);
+    table[2] = init_rare_table(table[2]);
+    table[3] = init_epic_table(table[3]);
+    table[4] = init_legendary_table(table[4]);
     return table;
 }
