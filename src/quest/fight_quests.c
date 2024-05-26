0711122ae4fdd184262d72a3b73049e1d887ade8
Azurioh <alan.cunin@epitech.eu>
2024-05-26 13:51:49 +0200

    (~) Fix fight quests

--- src/quest/fight_quests.c
+++ src/quest/fight_quests.c
@@ -11,7 +11,7 @@ void fight_quests(myrpg_t *myrpg)
 {
     if (QUESTS->quests[4]->is_validate == false && PLAYER->level >= 2) {
         QUESTS->quests[4]->nb++;
-        return update_quest(QUESTS);
+        update_quest(QUESTS);
     }
     for (int i = 2; i <= 8; i += 3) {
         if (QUESTS->quests[i]->is_unlocked == true
